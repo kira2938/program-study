2e990b7c649f346f5510f1782b66ab2b78dea9a4
SJ <51807692+kira2938@users.noreply.github.com>
2019-09-20 15:18:47 +0900

    Create urls.py

---
+++ scrape/web-django/web-django/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path(r'admin/', admin.site.urls),
+    path(r'tblog/', include('tblog.urls')),
+    
+]
