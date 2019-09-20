1b05e8c86d6f81f1acb258807df9f0c192e7ba1b
SJ <51807692+kira2938@users.noreply.github.com>
2019-09-20 15:16:30 +0900

    Create urls.py

---
+++ scrape/web-django/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+        path('', views.index, name='index'),
+        path('create', views.create, name='create'),
+        path('search', views.search, name='search'),
+        path('edit/<int:num>', views.edit, name='edit'),
+        path('delete/<int:num>', views.delete, name='delete'),
+        path('detail/<int:num>', views.detail, name='detail'),
+]
