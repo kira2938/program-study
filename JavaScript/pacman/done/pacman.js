88fcc9c6e89a386d5f45f40ec230c285e7530a9b
SJ <51807692+kira2938@users.noreply.github.com>
2019-10-15 17:18:43 +0900

    Update pacman.js

--- JavaScript/pacman/done/pacman.js
+++ JavaScript/pacman/done/pacman.js
@@ -1,23 +1,50 @@
 // パックマンの属性とともにコンストラクタを定義してPacmanという名前をつける
-var Pacman = function(cx, cy, speed, theta) {
-  this.position = [cx, cy];
-  this.moveingDirection = [0, 0];
+var Pacman = function(radius, speed, theta, map, row, col) {
+  // タイルベースの移動に必要な情報
+  this.map = map;
+
+  var rect = this.map.getTilePosition(row, col);
+
+  // ぷくセルベースの移動の描画に必要な情報
+  this.radius = radius;
+  this.position = {
+    x: Math.floor(rect.left + this.map.getTileWidth() / 2),
+    y: Math.floor(rect.top + this.map.getTileHeight() / 2)
+  };
+  this.moveingDirection = { x: 0, y: 0 };
   this.speed = speed;
   this.theta = theta;
+  this.dTheta = 3;
 };
-
-// 定義したPacmanコンストラクタにメソッドを定義
+// パックマンのメソッドを定義
+// 今のところ、メソッドはコンストラクタの持つporototypeオブジェクトのメソッドとして定義すると覚える
 Pacman.prototype = {
   getCx: function() {
-    return this.position[0];
+    return this.position.x;
   },
 
   getCy: function() {
-    return this.position[1];
+    return this.position.y;
   },
 
-  getPosition: function(i) {
-    return this.position[i];
+  getRadius: function() {
+    return this.radius;
+  },
+
+  goLeft: function() {
+    return this.getCx() - this.getRadius;
+  },
+
+  goRight: function() {
+    return this.getCx() + this.getRadius;
+  },
+
+  goTop: function() {
+    return this.getCy() - this.getRadius;
+  },
+
+  goBottom: function() {
+    return this.getCy() + this.getRadius;
   },
 
   getSpeed: function() {
@@ -28,27 +55,45 @@ Pacman.prototype = {
     return this.theta;
   },
 
-  go_left: function() {
-    this.movineDirection = [-1 * this.getSpeed(), 0];
+  goLeft: function() {
+    this.moveingDirection = { x: -1, y: 0 };
   },
 
-  go_right: function() {
-    this.movineDirection = [this.getSpeed(), 0];
+  goRight: function() {
+    this.moveingDirection = { x: 1, y: 0 };
   },
 
-  go_up: function() {
-    this.movineDirection = [0, -1 * this.getSpeed()];
+  goUp: function() {
+    this.moveingDirection = { x: 0, y: -1 };
   },
 
-  go_down: function() {
-    this.movineDirection = [0, this.getSpeed()];
+  goDown: function() {
+    this.moveingDirection = { x: 0, y: 1 };
   },
 
   move: function() {
     this.chew();
-    for (var i = 0; i < 2; i++) {
-      this.position[i] += this.movingDirection[i];
+
+    var previous_pos_x = this.position.x;
+    var previous_pos_y = this.position.y;
+
+    this.position.x +=
+      (this.moveingDirection.x * duration * this.getSpeed()) / 1000;
+
+    if (this.isInWall()) {
+      this.position.x = previous_pos_x;
     }
+
+    this.position.y +=
+      (this.moveingDirection.y * duration * this.getSpeed()) / 1000;
+
+    if (this.isInWall()) {
+      this.position.y = previous_pos_y;
+    }
+
+    // for (var i = 0; i < 2; i++) {
+    //   this.position[i] += this.moveingDirection[i];
+    // }
   },
 
   chew: function() {
@@ -60,21 +105,22 @@ Pacman.prototype = {
   },
 
   draw: function(ctx) {
-    RADIUS = 20;
+    // RADIUS = 20;
     ctx.strokeStyle = "#FF0000";
     ctx.fillStyle = "#FF0000";
     ctx.beginPath();
     ctx.arc(
       this.getCx(),
       this.getCy(),
-      RADIUS,
+      this.radius,
       (this.getTheta() * Math.PI) / 180,
       ((360 - this.getTheta()) * Math.PI) / 180
     );
     ctx.lineTo(this.getCx(), this.getCy());
     ctx.lineTo(
-      this.getCx() + RADIUS * Math.cos((this.getTheta() * Math.PI) / 180),
-      this.position[1] + RADIUS * Math.sin((this.getTheta() * Math.PI) / 180)
+      this.getCx() + this.radius * Math.cos((this.getTheta() * Math.PI) / 180),
+      this.position[1] +
+        this.radius * Math.sin((this.getTheta() * Math.PI) / 180)
     );
     ctx.fill();
   }
