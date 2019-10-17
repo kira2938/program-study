0fe4229d9ac759b292529638a16c2683f44f024e
SJ <51807692+kira2938@users.noreply.github.com>
2019-10-17 17:29:46 +0900

    Update map001.js

--- JavaScript/pacman/done/map001.js
+++ JavaScript/pacman/done/map001.js
@@ -4,11 +4,11 @@ var Map = function(startX, startY, tileWidth, tileHeight, wallFillStyle) {
   this.startY = startY;
 
   // タイルの大きさ
-  this.tileWidth = tileWidth;
-  this.tileHeight = tileHeight;
+  this.titleSize = { 'w': tileWidth, 'h': tileHeight };
 
   this.wallFillStyle = wallFillStyle; // タイルの塗りつぶし方法
 
+  // マップ 0 -> 壁 1 -> 通路
   this.map = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
@@ -40,44 +40,57 @@ Map.prototype = {
 
   // タイルの幅
   getTileWidth: function() {
-    return this.tileWidth;
+    return this.tileSize.w;
   },
 
   // タイルの高さ
   getTileHeight: function() {
-    return this.tileHeight;
+    return this.tileSize.h;
   },
 
   // ピクセルで表される座標の点(x, y)が含まれるタイルを返す
   getTile: function(x, y) {
     var col = Math.floor((x - this.startX) / this.getTileWidth());
     var row = Math.floor((y - this.startY) / this.getTileHeight());
-    return {'row': row, 'col': col, ' kind': this.map[row, col]};
+    return {'row': row, 'col': col, ' kind': this.map[row][col]};
   },
 
   // row行col例目のタイルのピクセルで表される座標の左上の点(x, y)を返す
-  getTilePosition: function(row, col) {
+  getTileLeftTop: function(row, col) {
     return {
-      left: this.startX + col * this.getTileWidth(),
-      top: this.startY + row * this.getTileHeight()
+      'left': this.startX + col * this.getTileWidth(),
+     ' top': this.startY + row * this.getTileHeight()
     };
   },
 
-  // ピクセルで表される座標の点(x, y)が壁のタイルであればtrueを返す
-  isWall: function(x, y) {
+  // ピクセルで表される座標の点(x, y)が属するタイルの左のタイルが壁であればtrueを返す
+  isLeftBlockWall: function(x, y) {
     var tile = this.getTile(x, y);
-    return tile.kind == 0;
+    return tile.col == 0 || this.map[tile.row][tile.col - 1] == 0;
   },
+
+  // ピクセルで表される座標の点(x, y)が属するタイルの上のタイルが壁であればtrueを返す
+  isAboveBlockWall: function (x, y) {
+    var tile = this.getTile(x, y);
+    return tile.row == 0 || this.map[tile.row - 1][tile.col] == 0;
+  },
+
+  // ピクセルで表される座標の点(x, y)が属するタイルの右のタイルが壁であればtrueを返す
+  isRightBlockWall: function (x, y) {
+    var tile = this.getTile(x, y);
+    return tile.col + 1 == this.getColLength() || this.map[tile.row][tile.col + 1] == 0;
+  },
+
+  // ピクセルで表される座標の点(x, y)が属するタイルの下のタイルが壁であればtrueを返す
+  isBelowBlockWall: function (x, y) {
+    var tile = this.getTile(x, y);
+    return tile.row + 1 == this.getRowLength || this.map[tile.row + 1][tile.col == 0;]
+  }
 
   // 壁の描画
   drawWall: function(ctx, row, col) {
     ctx.fillStyle = this.wallFillStyle;
-    ctx.rect(
-      this.startX + col * this.getTileWidth(),
-      this.startY + row * this.getTileHeight(),
-      this.getTileWidth(),
-      this.getTileHeight()
-    );
+    ctx.rect( this.startX + col * this.getTileWidth(), this.startY + row * this.getTileHeight(), this.getTileWidth(),  this.getTileHeight());
     ctx.fill();
   },
 
