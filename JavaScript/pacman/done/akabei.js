fcfcbbfe4424df564670d3cdffefa1f2cb00c123
SJ <51807692+kira2938@users.noreply.github.com>
2019-10-17 17:29:32 +0900

    Update akabei.js

--- JavaScript/pacman/done/akabei.js
+++ JavaScript/pacman/done/akabei.js
@@ -1,21 +1,69 @@
-var Akabei = function(cx, cy, speed) {
-  this.position = [cx, cy];
-  this.movingDirection = [0, 0];
-  this.speed = speed;
+var Akabei = function(radius, speed, color, pacman, map, row, col) {
+  Character.call(this, speed, map, row, col);
+  this.radius = radius;
+  this.color = color;
+  this.pacman = pacman;
 };
 
-Akabei.prototype = {
-  getCx: function() {
-    return this.position[0];
-  },
+inheritFromCharacter(Akabei);
 
-  getCy: function() {
-    return this.position[1];
-  },
+Akabei.prototype.getRadius = function () {
+  return this.radius;
+};
 
-  getSpeed: function() {
-    return this.speed;
-  },
+Akabei.prototype.getLeft = function () {
+  return this.getCx() - this.getRadius();
+};
+
+Akabei.prototype.getTop = function () {
+  return this.getCy() - this.getRadius();
+};
+
+Akabei.prototype.getRight = function () {
+  return this.getCx() + this.getRadius();
+};
+
+Akabei.prototype.getBottom = function () {
+  return this.getCy() + this.getRadius();
+};
+
+Akabei.prototype.decideDirection = function (duration) {
+  if (Math.random() < 0.5) {
+    if (this.pacman.getCx() - this.getCx() < 0) {
+      this.nextMovingDirection.x = -1;
+    } else {
+      this.nextMovingDirection.x = 1;
+    }
+    this.nextMovingDirection.y = 0;
+  } else {
+    this.nextMovingDirection.x = 0;
+    if (this.pacman.getCy() - this.getCy() < 0) {
+      this.nextMovingDirection.y = -1;
+    } else {
+      this.nextMovingDirection.y = 1;
+    }
+  }
+};
+
+Akabei.prototype.move = function (duration) {
+  this.decideDirection(duration);
+  Character.prototype.move.call(this, duration);
+};
+
+Akabei.prototype.killPacman = function () {
+  if (this.getDistance(this.pacman) <= Math.max(this.radius, this.pacman.radius)) {
+    this.pacman.die();
+  }
+};
+
+
+  // getCy: function() {
+  //   return this.position[1];
+  // },
+
+  // getSpeed: function() {
+  //   return this.speed;
+  // },
 
   move: function(pacman) {
     this.movingDirection[0] = this.movingDirection[1] = 0;
@@ -28,59 +76,38 @@ Akabei.prototype = {
     }
   },
 
-  draw: function(ctx) {
-    cx = this.getCx();
-    cy = this.getCy();
-    RADIUS = 20;
-    BODY_H = RADIUS;
-    LEG_H = (RADIUS / 5) * 2;
-    ctx.fillStyle = "#FF0000";
-    ctx.beginPath();
-    ctx.moveTo(cx - RADIUS, cy);
-    ctx.arc(cx, cy, RADIUS, Math.PI, 2 * Math.PI);
-    ctx.lineTo(cx + RADIUS, cy + BODY_H);
-    ctx.lineTo(cx + (RADIUS / 3) * 2, cy + BODY_H - LEG_H);
-    ctx.lineTo(cx + RADIUS / 3, cy + BODY_H);
-    ctx.lineTo(cx, cy + BODY_H - LEG_H);
-    ctx.lineTo(cx + RADIUS / 3, cy + BODY_H);
-    ctx.lineTo(cx - (RADIUS / 3) * 2, cy + BODY_H - LEG_H);
-    ctx.lineTo(cx - RADIUS, cy + BODY_H);
-    ctx.lineTo(cx - RADIUS, cy);
-    ctx.fill();
+Akabei.prototype.draw = function(ctx) {
+  var cx = this.getCx();
+  var cy = this.getCy();
+  var radius = this.radius();
+  var bodyHeight =radius;
+  var legHeight = radius / 5 * 2;
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.moveTo(cx - radius, cy);
+  ctx.arc(cx, cy, radius, Math.PI, 2 * Math.PI);
+  ctx.lineTo(cx + radius, cy + bodyHeight);
+  ctx.lineTo(cx + radius / 3 * 2, cy + bodyHeight - legHeight);
+  ctx.lineTo(cx + radius / 3, cy + bodyHeight);
+  ctx.lineTo(cx, cy + bodyHeight - legHeight);
+  ctx.lineTo(cx + radius / 3, cy + bodyHeight);
+  ctx.lineTo(cx - radius / 3 * 2, cy + bodyHeight - legHeight);
+  ctx.lineTo(cx - radius, cy + bodyHeight);
+  ctx.lineTo(cx - radius, cy);
+  ctx.fill();
     // ctx.stroke();
-    ctx.fillStyle = "#FFFFFF";
-    ctx.beginPath();
-    ctx.arc(
-      cx - (20 * RADIUS) / 50,
-      cy - 4,
-      (16 * RADIUS) / 50,
-      0,
-      Math.PI * 2
-    );
-    ctx.arc(
-      cx + (20 * RADIUS) / 50,
-      cy - 4,
-      (16 * RADIUS) / 50,
-      0,
-      Math.PI * 2
-    );
-    ctx.fill();
-    ctx.fillStyle = "#0000FF";
-    ctx.beginPath();
-    ctx.arc(
-      cx + ((-20 - 8) * RADIUS) / 50,
-      cy - (4 * RADIUS) / 50,
-      (7 * RADIUS) / 50,
-      0,
-      Math.PI * 2
-    );
-    ctx.arc(
-      cx + ((20 - 8) * RADIUS) / 50,
-      cy - (4 * RADIUS) / 50,
-      (7 * RADIUS) / 50,
-      0,
-      Math.PI * 2
-    );
-    ctx.fill();
-  }
+  ctx.fillStyle = "#FFFFFF";
+  var leftEyeX = cx - 20 * radius / 50;
+  var leftEyeY = cy - 4 * radius / 50;
+  var rightEyeX = cx + 20 * radius / 50;
+  var rightEyeY = cy + 4 * radius / 50;
+  ctx.beginPath();
+  ctx.arc(leftEyeX, leftEyeY, 16 * radius / 50, 0, Math.PI * 2);
+  ctx.arc(rightEyeX, rightEyeY, 16 * radius / 50, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.fillStyle = "#0000FF";
+  ctx.beginPath();
+  ctx.arc(leftEyeX - 8 * radius / 50, leftEyeY - 4 * radius / 50, 7 * radius / 50, 0, Math.PI * 2);
+  ctx.arc(rightEyeX - 8 * radius / 50, rightEyeY - 4 * radius / 50, 7 * radius / 50, 0, Math.PI * 2);
+  ctx.fill();
 };
